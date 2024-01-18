4baad8b214f01e9e95a5b784cc29252c422457f9
21Alul21 <augustine.alul@yahoo.com>
2024-01-18 22:55:55 +0100

    mysql advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- sql script that creates a table 'users'
 CREATE TABLE IF NOT EXISTS users
-(id INT AUTO_INCREMENT NOT NULL,
-email VARCHAR(255) NOT NULL UNIQU,
+(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255),
 country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );   
